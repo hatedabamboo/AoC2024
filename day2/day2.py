a7d04a3942e12ecd3d72ff1a54e1ef074b1ba890
hatedabamboo <hatedabamboo@pm.me>
2024-12-02 22:34:20 +0100

    Day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -2,18 +2,35 @@ filename = "day2/day2task.txt"
 
 safe_reports = 0
 
+
+def is_increasing(lst: list[int]) -> bool:
+    return all(lst[i] < lst[i + 1] for i in range(len(lst) - 1))
+
+
+def is_decreasing(lst: list[int]) -> bool:
+    return all(lst[i] > lst[i + 1] for i in range(len(lst) - 1))
+
+
+def is_gradual(lst: list[int]) -> bool:
+    difference = [lst[i + 1] - lst[i] for i in range(len(lst) - 1)]
+    return all(abs(diff) <= 3 for diff in difference)
+
+
+def is_popping_helps(lst: list[int]) -> bool:
+    for i in range(len(lst)):
+        list_to_iterate = lst[:i] + lst[i + 1 :]
+        if is_increasing(list_to_iterate) or is_decreasing(list_to_iterate):
+            if is_gradual(list_to_iterate):
+                return True
+    return False
+
+
 with open(filename, "r", encoding="UTF-8") as file:
     while line := file.readline():
-        lst = list(map(int, line.strip().split(' ')))
-        if all(lst[i] < lst[i+1] for i in range(len(lst) -1)): # increasing
-            difference = [lst[i+1] - lst[i] for i in range(len(lst) - 1)]
-            if all(abs(diff) <= 3 for diff in difference):
-                safe_reports += 1
-        elif all(lst[i+1] < lst[i] for i in range(len(lst) -1)): # increasing
-            difference = [lst[i] - lst[i+1] for i in range(len(lst) - 1)]
-            if all(diff <= 3 for diff in difference):
-                safe_reports += 1
-        else:
-            continue
+        lst = list(map(int, line.strip().split(" ")))
+        if (is_increasing(lst) or is_decreasing(lst)) and is_gradual(lst):
+            safe_reports += 1
+        elif is_popping_helps(lst):
+            safe_reports += 1
 
 print(safe_reports)
